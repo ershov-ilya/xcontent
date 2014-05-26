ea4938523eafff1d679d5d0258ca597000694c04
ILYA ERSHOV <ershov.ilya@gmail.com>
2014-05-26 14:57:11 +0400

    Fixed errors.

--- core/components/xcontent/elements/snippets/xcontent.snippet.php
+++ core/components/xcontent/elements/snippets/xcontent.snippet.php
@@ -7,18 +7,19 @@ include_once (MODX_ASSETS_PATH .'components/xcontent/xcontent.config.php');
 //read text
 $content = $modx->resource->get('content');
 // check config
-// $disable = $modx->resource->getTVValue(23);
-$disable = false;
+$disable = $modx->resource->getTVValue(5);
+//$disable = false;
 if(!isset($REG) || $disable) { print $content; return '';}
 $arr = explode("\n", $content);
 
 // Settings
-define(DEBUG, 0);
-define(SKIP_LINES, 0);
-define(LINKS_LIMIT, 4);
+define('DEBUG', 0);
+define('SKIP_LINES', 0);
+define('LINKS_LIMIT', 4);
+$output='';
 
 
-if(DEBUG) print "<pre>";
+if(DEBUG) $output.= "<pre>";
 // Начинаем разбор
 $links=0;
 $str_num=1;
@@ -31,6 +32,7 @@ foreach($arr as $str)
     $skip=0;
     if(preg_match('/<a.*?>/', $str)) $skip=1;
     if(preg_match('/<img.*?>/', $str)) $skip=1;
+    if(preg_match('/<h[1-8]{1}/', $str)) $skip=1;
 
     // Также пропускаем первую строку и если лимит ссылок уже достигнут
     if($links < LINKS_LIMIT && !$skip && $str_num>SKIP_LINES)
@@ -40,11 +42,11 @@ foreach($arr as $str)
             $res = preg_match($reg, $str, $match);
             if($res==0) {  $i++; continue; }
 
-            if(DEBUG) print "\n -------------------- \n(i=$i) "."Str: ".$str."\n";;
+            if(DEBUG) $output.= "\n -------------------- \n(i=$i) "."Str: ".$str."\n";;
 
             if($res)
             {
-                if(DEBUG) print "$links) Found: ".$match[1]."\n";
+                if(DEBUG) $output.= "$links) Found: ".$match[1]."\n";
 
                 // Replace
                 //$tag = "<a href='$LINK[$i]'>$match[1]</a>".$match[2];
@@ -52,15 +54,17 @@ foreach($arr as $str)
                 $str = preg_replace($REG[$i], $tag, $str, 1);
             }
             $links++;
-            if(DEBUG) print "links = $links\n";
+            if(DEBUG) $output.= "links = $links\n";
             break;
         }
     }
 
-    if(DEBUG) print ">> ";
+    if(DEBUG) $output.= ">> ";
     //print $str."\n";
-    print $str;
+    $output.= $str;
     $str_num++;
 }
 /**/
-if(DEBUG) print "</pre>";
+if(DEBUG) $output.= "</pre>";
+
+return $output;
