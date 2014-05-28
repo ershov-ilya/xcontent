5b9558aa652f5370d8f728211aa87c046bb42990
ILYA ERSHOV <ershov.ilya@gmail.com>
2014-05-28 09:01:23 +0400

    Update xcontent.snippet.php

--- core/components/xcontent/elements/snippets/xcontent.snippet.php
+++ core/components/xcontent/elements/snippets/xcontent.snippet.php
@@ -1,4 +1,29 @@
 <?php
+ /**
+ * Project: xContent
+ * Date: 28.05.14
+ * Time: 8:56
+ * Author:  ILYA ERSHOV
+ * http://about.me/ershov.ilya
+ * GitHub:  https://github.com/ershov-ilya
+ * Edited in PhpStorm.
+ */
+
+// Init
+$defconfig = array(
+    'id' => $modx->resource->get('id'),
+    'context' => $modx->context->key,
+    'links_limit' => 3,
+    'skip_lines' => 0,
+    'skip_page_tv_id' => 8
+);
+$config = array_merge($defconfig, $scriptProperties);
+
+define('DEBUG', 0);
+define('SKIP_LINES', $config['skip_lines']); // $config['skip_lines']
+define('LINKS_LIMIT', $config['links_limit']); // $config['links_limit']
+$output='';
+
 include_once (MODX_ASSETS_PATH .'components/xcontent/xcontent.config.php');
 
 /* @var modX $modx */
@@ -7,16 +32,10 @@ include_once (MODX_ASSETS_PATH .'components/xcontent/xcontent.config.php');
 //read text
 $content = $modx->resource->get('content');
 // check config
-$disable = $modx->resource->getTVValue(5);
-//$disable = false;
+$disable = $modx->resource->getTVValue($config['skip_page_tv_id']);
 if(!isset($REG) || $disable) { return $content;}
 $arr = explode("\n", $content);
 
-// Settings
-define('DEBUG', 0);
-define('SKIP_LINES', 0);
-define('LINKS_LIMIT', 4);
-$output='';
 
 
 if(DEBUG) $output.= "<pre>";
@@ -30,8 +49,8 @@ foreach($arr as $str)
 
     // Пропускаем строки уже содержащие ссылки или картинки
     $skip=0;
-    if(preg_match('/<a.*?>/', $str)) $skip=1;
-    if(preg_match('/<img.*?>/', $str)) $skip=1;
+    if(preg_match('/<a\s/', $str)) $skip=1;
+    if(preg_match('/<img\s/', $str)) $skip=1;
     if(preg_match('/<h[1-8]{1}/', $str)) $skip=1;
 
     // Также пропускаем первую строку и если лимит ссылок уже достигнут
@@ -49,7 +68,6 @@ foreach($arr as $str)
                 if(DEBUG) $output.= "$links) Found: ".$match[1]."\n";
 
                 // Replace
-                //$tag = "<a href='$LINK[$i]'>$match[1]</a>".$match[2];
                 $tag = "<a href='$LINK[$i]'>$match[1]</a>";
                 $str = preg_replace($REG[$i], $tag, $str, 1);
             }
@@ -60,7 +78,6 @@ foreach($arr as $str)
     }
 
     if(DEBUG) $output.= ">> ";
-    //print $str."\n";
     $output.= $str;
     $str_num++;
 }
